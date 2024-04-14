df6055fbf9c82675effb94e0a5e63c772a6b6120
michaelamando1 <michaelamando25@gmail.com>
2024-04-14 20:58:20 +0100

    Additional

--- random.js
+++ random.js
@@ -1,3 +1,13 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 console.log(randomNumber);
+
+// Code to generate Random Letters
+
+function getRandomLowercaseLetter() {
+    let randomCharCode = Math.floor(Math.random() * 26) + 97; // ASCII for a-z is 97-122
+    return String.fromCharCode(randomCharCode);
+  }
+  
+  let randomLowercaseLetter = getRandomLowercaseLetter();
+  console.log(randomLowercaseLetter);
